c006a48879c2de1913bee4cb92325a0015713cea
SegunAriyo <segun.ariyo@globalaccelerex.com>
2024-08-01 13:27:48 +0100

    pushing a bit of refinement

--- src/features/client/pages/home/data.tsx
+++ src/features/client/pages/home/data.tsx
@@ -1,4 +1,3 @@
-
 import ReusableCountUp from "../../../../utils";
 export const data = [
   {
@@ -52,55 +51,101 @@ export const idileMajemu = [
 // ];
 
 export const BoardData = [
-
-
-  { name: 'Adefoluke Adeyemo', position: 'Position 1', imageUrl: '../../../../../public/assets/images/Adefoluke Adeyemo.jpg' },
-  { name: 'Adesola Afun-Ogidan', position: 'Position 2', imageUrl: '../../../../../public/assets/images/Adesola Afun-Ogidan.jpg' },
-  { name: 'Adesola Hannah Ajakaiye', position: 'Position 3', imageUrl: '../../../../../public/assets/images/Hannah Ajakaiye.jpg' },
-  { name: 'Idris Balogun', position: 'Position 4', imageUrl: '../../../../../public/assets/images/Idris Balogun.jpg' },
-  { name: 'Olawale Alo', position: 'Position 5', imageUrl: '../../../../../public/assets/images/Olawale Alo.jpg' },
-  { name: 'Olayiwola Ajilore', position: 'Position 6', imageUrl: '../../../../../public/assets/images/Olayiwola Ajilore.jpg' },
-  { name: 'TOlufunke Okejimi', position: 'Position 7', imageUrl: '../../../../../public/assets/images/Olufunke Okejimi.jpg' },
-  { name: 'Temitayo Odeyemi', position: 'Position 8', imageUrl: '../../../../../public/assets/images/Temitayo Odeyemi.jpg' },
-  { name: 'Victor Ogunleye', position: 'Position 9', imageUrl: '../../../../../public/assets/images/Victor Ogunleye.jpg' },
-
-
+  {
+    name: "Dr. Temitayo Odeyemi",
+    linkedInUrl: "https://www.linkedin.com/in/temitayo-isaac-odeyemi-1943b553/",
+    profile:
+      "Dr. Temitayo Odeyemi is a lecturer and researcher with specialisation in Politics and International Studies.",
+    imageUrl: "../../../../../public/assets/images/Temitayo Odeyemi.jpg",
+  },
+  {
+    name: "Adesola Afun-Ogidan",
+    linkedInUrl: "https://www.linkedin.com/in/afun-ogidan-adesola-45159693/",
+    profile:
+      "Adesola Afun-Ogidan is a marketing professional with over 10 years of experience across industries.",
+    imageUrl: "../../../../../public/assets/images/Adesola Afun-Ogidan.jpg",
+  },
+  {
+    name: "Olawale Alo",
+    linkedInUrl: "https://www.linkedin.com/in/olawale-alo-9651a125/",
+    profile:
+      "Olawale Alo is a Geology PhD Candidate at the School of Geosciences, University of Oklahoma, USA.",
+    imageUrl: "../../../../../public/assets/images/Olawale Alo.jpg",
+  },
+  {
+    name: "Victor Ogunleye",
+    linkedInUrl: "https://www.linkedin.com/in/victor-ogunleye-acipm-01a1b68a/",
+    profile:
+      "Victor Ogunleye is a Talent Engagement Manager at Guinness Nigeria Plc.",
+    imageUrl: "../../../../../public/assets/images/Victor Ogunleye.jpg",
+  },
+  {
+    name: "Olufunke Okejimi",
+    linkedInUrl: "https://www.linkedin.com/in/olufunke-janet-aleji",
+    profile:
+      "Olufunke Okejimi is a Digital Innovation Officer with a dynamic background in customer management.",
+    imageUrl: "../../../../../public/assets/images/Olufunke Okejimi.jpg",
+  },
+  {
+    name: "Olayiwola Ajilore",
+    linkedInUrl: "https://www.linkedin.com/in/olayiwola-ajilore-551b6a31/",
+    profile:
+      "Olayiwola Ajilore is the Service Director for GE Vernova Gas Power Services in Nigeria.",
+    imageUrl: "../../../../../public/assets/images/Olayiwola Ajilore.jpg",
+  },
+  {
+    name: "Idris Balogun",
+    linkedInUrl: "https://www.linkedin.com/in/idris-omobemi-balogun-167838145/",
+    profile: "Idris Balogun is a Legal Practitioner by profession.",
+    imageUrl: "../../../../../public/assets/images/Idris Balogun.jpg",
+  },
+  {
+    name: "Adefoluke Adeyemo",
+    linkedInUrl: "https://www.linkedin.com/in/adefoluke-adeyemo/",
+    profile:
+      "Adefoluke Adeyemo is a Human Resources Practitioner at FMDQ Group PLC.",
+    imageUrl: "../../../../../public/assets/images/Adefoluke Adeyemo.jpg",
+  },
+  {
+    name: "Adesola Hannah Ajakaiye",
+    linkedInUrl: "https://www.linkedin.com/in/hannah-ajakaiye-4b993a8a/",
+    profile:
+      "Hannah Ajakaiye is the founder of FactsMatterNG.",
+    imageUrl: "../../../../../public/assets/images/Hannah Ajakaiye.jpg",
+  },
 ];
-
 
 export const TestimonialData = [
   {
     name: "Ijitayo Abosede Amoke",
-    text: "The Isokan Bursary Scheme was a lifesaver for me. Initially, I applied for minor needs, but it helped greatly when school fees increased significantly. It eased the financial burden on my parents, allowing us to manage other expenses. I am very grateful for the support."
+    text: "The Isokan Bursary Scheme was a lifesaver for me. Initially, I applied for minor needs, but it helped greatly when school fees increased significantly. It eased the financial burden on my parents, allowing us to manage other expenses. I am very grateful for the support.",
   },
   {
     name: "Kolade Adegbola",
-    text: "The Isokan Bursary Scheme helped me pay my school fees during tough financial times. It provided timely relief and allowed my family to use their limited funds for other necessities. I am deeply grateful to the donors and trustees for their support during those challenging periods."
+    text: "The Isokan Bursary Scheme helped me pay my school fees during tough financial times. It provided timely relief and allowed my family to use their limited funds for other necessities. I am deeply grateful to the donors and trustees for their support during those challenging periods.",
   },
   {
     name: "Victor Babarinde",
-    text: "The Ìdílé Majẹmu Foundation’s bursary scheme significantly eased my financial burden during my undergraduate years. It helped pay my tuition fees during tough economic times and allowed my family to manage better. I am grateful to the foundation and my local church for their support."
+    text: "The Ìdílé Majẹmu Foundation’s bursary scheme significantly eased my financial burden during my undergraduate years. It helped pay my tuition fees during tough economic times and allowed my family to manage better. I am grateful to the foundation and my local church for their support.",
   },
   {
     name: "Oludare Ibukunoluwa Toluwalase",
-    text: "The Isokan Bursary Scheme helped me pay my school fees during a challenging academic session. It eased the financial burden on my parents and contributed to my professional qualifications. I am forever grateful to Idile Majemu and Isokan OAU for their support."
+    text: "The Isokan Bursary Scheme helped me pay my school fees during a challenging academic session. It eased the financial burden on my parents and contributed to my professional qualifications. I am forever grateful to Idile Majemu and Isokan OAU for their support.",
   },
   {
     name: "Adedeji Adetoun Deborah",
-    text: "The Isokan Bursary Scheme was a great help when my school fees increased by almost 200%. It allowed me to pay my tuition fees at once, easing the financial stress on my parents. I am grateful to the Idile Majemu Human Development Foundation for their support."
+    text: "The Isokan Bursary Scheme was a great help when my school fees increased by almost 200%. It allowed me to pay my tuition fees at once, easing the financial stress on my parents. I am grateful to the Idile Majemu Human Development Foundation for their support.",
   },
   {
     name: "Shobande Maria",
-    text: "Receiving the Isokan Bursary twice helped reduce my financial burdens significantly. It allowed me to focus on my academics and other essential needs without worrying about school fees. I am very thankful to the bursary scheme for their support during my academic journey."
+    text: "Receiving the Isokan Bursary twice helped reduce my financial burdens significantly. It allowed me to focus on my academics and other essential needs without worrying about school fees. I am very thankful to the bursary scheme for their support during my academic journey.",
   },
   {
     name: "Akeredolu Iyanu Abiodun",
-    text: "The bursary provided crucial financial support, allowing me to focus on my studies and obtain necessary learning materials. It motivated me to aim for academic excellence and showed the importance of community support. I am very thankful to the Isokan Bursary Scheme for their invaluable assistance."
+    text: "The bursary provided crucial financial support, allowing me to focus on my studies and obtain necessary learning materials. It motivated me to aim for academic excellence and showed the importance of community support. I am very thankful to the Isokan Bursary Scheme for their invaluable assistance.",
   },
   {
     name: "Ekundayo Temitope Esther",
-    text: "Being awarded the Isokan Bursary twice was a great blessing. It eased my financial and academic pressures, allowing me to focus on excelling in my studies. I am grateful to the Idile Majemu Human Development Foundation for their support, which relieved my parents of some financial burdens."
-  }
-]
-
-
+    text: "Being awarded the Isokan Bursary twice was a great blessing. It eased my financial and academic pressures, allowing me to focus on excelling in my studies. I am grateful to the Idile Majemu Human Development Foundation for their support, which relieved my parents of some financial burdens.",
+  },
+];
